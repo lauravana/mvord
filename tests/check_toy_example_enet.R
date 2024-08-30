0ef0b6642f37222e830461f07de0b00977afaa9a
Laura Vana <laura.vana.guer@tuwien.ac.at>
2024-08-30 15:06:40 +0200

    Update check_toy_example_enet.R

--- tests/check_toy_example_enet.R
+++ tests/check_toy_example_enet.R
@@ -6,12 +6,12 @@ library(MASS)
 tolerance <- 1e-6
 
 mult.obs <- 2
-nobs <- 100
+
 
 sigma <- matrix(c(1,0.8,0.8,1), ncol = 2)
 
 thresholds <- list(c(-1,1),c(-1,1))
-nobs <- 100
+nobs <- 1000
 suppressWarnings(RNGversion("3.5.0"))
 set.seed(2017)
 errors <-  mvrnorm(n = nobs, mu = rep(0, mult.obs), Sigma = sigma)
@@ -44,40 +44,6 @@ data_toy_example <- cbind.data.frame(y, predictors.fixed[[1]])
 colnames(data_toy_example) <- c("Y1","Y2", paste0("X", 1:p))
 w <- c(rep(1/20, 20), rep(1/30, 30), rep(1/20, 20), rep(1/30, 30))
 
-
-
-# convert data_toy_example into long format
-df <- cbind.data.frame("i" = rep(1:100,2),
-                       "j" = rep(1:2,each = 100),
-                       "Y" = c(data_toy_example$Y1, data_toy_example$Y2),
-                       "X1" = rep(data_toy_example$X1,2),
-                       "X2" = rep(data_toy_example$X2,2),
-                       "f1" = factor(sample(rep(data_toy_example$Y2,2)),
-                                     ordered = FALSE),
-                       "f2" = factor(rep(data_toy_example$Y1,2), ordered = FALSE),
-                       w    = rep(w,2))
-df$X3 <- cut(df$X2, c(-Inf, -0.2, 0.2, Inf))
-
-
-
-# library(ROI)
-# ROI_solver <- function(starting.values, objFun, control){
-#     n <- length(starting.values)
-#     op <- OP(objective = F_objective(objFun, n = n),
-#              bounds = V_bound(li = seq_len(n), lb = rep.int(-Inf, n)))
-#     optRes <- ROI_solve(op, solver = "nlminb",
-#         control = c(list(start = starting.values),
-#                     control))
-#     list(optpar    = optRes$solution,
-#          objective = optRes$objval) # a vector of length equal to number of parameters to optimize
-# }
-#
-#
-#
-
-# Test MMO() ----
-
-## Coef constraints as vector ----
 formula <- paste0("MMO2(Y1, Y2) ~ 0 + ", paste0("X", 1:p, collapse = " + "))
 res_mvord <- mvord:::mvord(formula = as.formula(formula),
                               data = data_toy_example,
@@ -106,7 +72,7 @@ compute_lambda_max <- function(y, X, alpha) {
 }
 
 ## TODO determine via coordinate descent
-lambda_max <- 1000 # compute_lambda_max(y, scale(pred), alpha = 0)
+lambda_max <- 1000 # compute_lambda_max(y, scale(pred), alpha = 1)
 lambda_min_ratio <- 0.0001
 lambda_min <- lambda_max * lambda_min_ratio
 lambda_grid <- exp(seq(log(lambda_max),
@@ -114,14 +80,14 @@ lambda_grid <- exp(seq(log(lambda_max),
 claic_vec_ridge <- NULL
 beta_mat_ridge <- matrix(nrow=p,ncol=0)
 for (i in seq_along(lambda_grid)) {
-  ## warm starts
+  ## warm starts don't work well as they cause false convergence
   lambda <- lambda_grid[i]
-  if (i == 1) {
-    start_vals <- NULL
-  } else {
-    start_vals <- list(theta=unique(unlist(res_ridge$theta)),
-                       beta=res_ridge$beta)
-  }
+  # if (i == 1) {
+  start_vals <- NULL
+  # } else {
+  #   start_vals <- list(theta=res_ridge$theta[[1]],
+  #                      beta=res_ridge$beta)
+  # }
   res_ridge <- mvord:::mvordnet(formula = as.formula(formula),
                                 data = data_toy_example,
                                 link = mvprobit(),
@@ -139,26 +105,29 @@ for (i in seq_along(lambda_grid)) {
 
 claic_vec_lasso <- NULL
 beta_mat_lasso <- matrix(nrow=p,ncol=0)
-lambda <- lambda_grid[1]
-for (lambda in lambda_grid) {
-  ## warm starts
+for (i in seq_along(lambda_grid)) {
+  ## warm starts don't work well as they cause false convergence
+  lambda <- lambda_grid[i]
+  # if (i == 1) {
   start_vals <- NULL
-  print(lambda)
-  res_lasso <- mvord:::mvordnet(formula = as.formula(formula),
-                                data = data_toy_example,
-                                link = mvprobit(),
-                                alpha = 1L,
-                                lambda = lambda,
-                                error.structure = cor_general(~1),
-                                threshold.constraints = c(1,1),
-                                coef.constraints = c(1,1),
-                                control= mvord.control(solver="Nelder-Mead",
-                                                       solver.optimx.control = list(trace=1),
-                                                       se=TRUE))
+  # } else {
+  #   start_vals <- list(theta=res_ridge$theta[[1]],
+  #                      beta=res_ridge$beta)
+  # }
+  res_lasso <- mvord:::mvordnet(
+    formula = as.formula(formula),
+    data = data_toy_example,
+    link = mvprobit(),
+    alpha = 1L,
+    lambda = lambda,
+    error.structure = cor_general(~1),
+    threshold.constraints = c(1,1),
+    coef.constraints = c(1,1),
+    control= mvord.control(solver="Nelder-Mead",
+                           solver.optimx.control = list(trace=1),
+                           se=TRUE))
   claic_vec_lasso <- c(claic_vec_lasso, AIC(res_lasso))
   beta_mat_lasso <- cbind(beta_mat_lasso, coef(res_lasso))
 }
-beta_mat_lasso[abs(beta_mat_lasso) < 1e-07] <- 0
-plot(log(lambda_grid), claic_vec_lasso)
-
-res_mvord$beta
+plot(claic_vec_lasso)
+beta_mat_lasso
